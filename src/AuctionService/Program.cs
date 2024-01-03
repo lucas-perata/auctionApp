3e799cd3edadbafa65b899fabb61af7350275430
lucas-perata <contacto.perata@gmail.com>
2024-01-03 13:54:51 -0300

    Seed data

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -25,4 +25,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+try 
+{
+    DbInitializer.InitDb(app); 
+}
+catch (Exception e)
+{
+    Console.WriteLine(e); 
+}
+
 app.Run();
--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8104b743597639d5a040de680b8be8e29147dee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
