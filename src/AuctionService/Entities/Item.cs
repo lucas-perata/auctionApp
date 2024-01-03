bb930ec147ff27e48f63027bffb89a5832b3af05
lucas-perata <contacto.perata@gmail.com>
2024-01-03 12:09:38 -0300

    Database connection, docker and entities auction

---
+++ src/AuctionService/Entities/Item.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities
+{
+    [Table("Items")]
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        // nav properties
+        public Auction Auction { get; set; }
+        public Guid AuctionId { get; set; }
+    }
+}
