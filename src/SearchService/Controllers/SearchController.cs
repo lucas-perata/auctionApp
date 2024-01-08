cdd862711db0e0efe88862488c63fd233a2c2f14
lucas-perata <contacto.perata@gmail.com>
2024-01-08 10:04:56 -0300

    Search filterparams and ignore

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers
 {
@@ -9,20 +10,55 @@ namespace SearchService.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
         {
-            var query = DB.Find<Item>(); 
+            var query = DB.PagedSearch<Item, Item>(); 
 
-            query.Sort(x => x.Ascending(a => a.Title)); 
-
-            if(!string.IsNullOrEmpty(searchTerm))
+            if(!string.IsNullOrEmpty(searchParams.searchTerm))
             {
-                query.Match(Search.Full, searchTerm).SortByTextScore();
+                query.Match(Search.Full, searchParams.searchTerm).SortByTextScore();
             }
+
+            query = searchParams.OrderBy switch 
+            {
+                "categoryName" =>  query.Sort(x => x.Ascending(a => a.CategoryName)),
+                "new" => query.Sort(x => x.Ascending(a => a.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+            };  
+
+            query = searchParams.FilterBy switch 
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(12) && x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            }; 
+
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query.Match(x => x.Seller == searchParams.Seller);
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query.Match(x => x.CategoryName == searchParams.Winner);
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.CategoryName))
+            {
+                query.Match(x => x.CategoryName == searchParams.CategoryName);
+            }
+
+            query.PageNumber(searchParams.pageNumber);
+            query.PageSize(searchParams.pageSize);
 
             var result = await query.ExecuteAsync(); 
 
-            return result; 
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            }); 
         }
     }
 }
--- src/SearchService/Models/Item.cs
+++ src/SearchService/Models/Item.cs
@@ -21,5 +21,7 @@ namespace SearchService.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+
+        public string CategoryName { get; set; }
     }
 }
