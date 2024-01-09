0bcacff6dfb32fb5a0910f7812ac5913007856a1
lucas-perata <contacto.perata@gmail.com>
2024-01-09 10:59:18 -0300

    Configuration rabbitmq, masstransit and contracts folder

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService.Data;
@@ -9,6 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context); 
+    });
+});
 
 var app = builder.Build();
 
